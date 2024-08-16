be9b54a8df311b36e5b780cf0b0d2b87c6069b7b
akfldk1028 <hanvit4303@gmail.com>
2024-08-16 18:27:34 +0900

    docker.

--- download_models.py
+++ download_models.py
@@ -3,7 +3,7 @@ from huggingface_hub import snapshot_download
 
 def download_models():
     # Hugging Face의 모델 저장소 이름
-    repo_id = "Amminity/motive"
+    repo_id = "Ammunity/motive"
 
     # 모델 다운로드
     snapshot_download(repo_id, local_dir="./models")
@@ -11,3 +11,14 @@ def download_models():
 
 if __name__ == "__main__":
     download_models()
+
+
+# from huggingface_hub import HfApi
+#
+# api = HfApi()
+# repo_id = "Amminity/motive"
+# try:
+#     repo_info = api.repo_info(repo_id=repo_id)
+#     print(repo_info)
+# except Exception as e:
+#     print(e)
